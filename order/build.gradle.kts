56db0492354f5f7dbea0203a745b6e488dd86543
jungchanSon <rnrmfjc@gmail.com>
2025-01-22 10:24:31 +0900

    build: add mapstruct

--- order/build.gradle.kts
+++ order/build.gradle.kts
@@ -16,5 +16,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:4.1.3")
     runtimeOnly("com.mysql:mysql-connector-j")
+
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+
 }
 
--- shop/build.gradle.kts
+++ shop/build.gradle.kts
@@ -18,6 +18,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     runtimeOnly("com.mysql:mysql-connector-j")
+
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+
 }
 
 dependencyManagement {
