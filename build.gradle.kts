8dd97c76541b27420384c6f3c34ed3eeaffa12d3
jungchanSon <rnrmfjc@gmail.com>
2025-01-22 16:51:37 +0900

    build: add spring-actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ subprojects {
 
 	dependencies {
 		implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+		implementation("org.springframework.boot:spring-boot-starter-actuator")
 		implementation("org.jetbrains.kotlin:kotlin-reflect")
 		compileOnly("org.projectlombok:lombok")
 		annotationProcessor("org.projectlombok:lombok")
